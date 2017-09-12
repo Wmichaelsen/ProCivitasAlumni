d26ee39a2d6650a34b4986ed071e0bc6e3064410
Wilhelm Michaelsen <wilhelmmichaelsen@Wilhelms-MacBook-Pro.local>
2017-09-12 07:56:10 +0200

    first commit

---
+++ ProCivitasAlumni/Alumni.swift
@@ -0,0 +1,9 @@
+//
+//  Alumni.swift
+//  ProCivitasAlumni
+//
+//  Created by Wilhelm Michaelsen on 2017-09-01.
+//  Copyright © 2017 Wilhelm Michaelsen. All rights reserved.
+//
+
+import Foundation
---
+++ ProCivitasAlumni/AlumniViewController.swift
@@ -0,0 +1,35 @@
+//
+//  AlumniViewController.swift
+//  ProCivitasAlumni
+//
+//  Created by Wilhelm Michaelsen on 2017-09-07.
+//  Copyright © 2017 Wilhelm Michaelsen. All rights reserved.
+//
+
+import UIKit
+
+class AlumniViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
